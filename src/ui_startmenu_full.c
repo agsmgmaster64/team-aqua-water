fd955a52f7c1d8769c748f900df58f226d293aea
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2024-07-19 23:56:57 -0400

    lil tweaks

--- src/ui_startmenu_full.c
+++ src/ui_startmenu_full.c
@@ -963,6 +963,7 @@ static void StartMenuFull_VBlankCB(void)
     LoadOam();
     ProcessSpriteCopyRequests();
     TransferPlttBuffer();
+    ChangeBgX(2, 128, BG_COORD_SUB); // controls the background scrolling
     ChangeBgY(2, 128, BG_COORD_SUB); // controls the background scrolling
 }
 
